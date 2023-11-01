08ee7ecc8cd030fd5f551601ce881a907b51df24
Dominik Vagala <37017489+dvagala@users.noreply.github.com>
2023-11-01 18:01:14 +0100

    Add files via upload

---
+++ try_the_colors_lib_poc.framework/Headers/try_the_colors_lib_poc-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "try_the_colors_poc.hpp"
+
+FOUNDATION_EXPORT double try_the_colors_lib_pocVersionNumber;
+FOUNDATION_EXPORT const unsigned char try_the_colors_lib_pocVersionString[];
+
